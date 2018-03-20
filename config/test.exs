d649a3de9fdbc6e7885b41b5c8407348c9a6c6c3
Tyler <tyler@nanoleaf.me>
2018-03-20 17:49:45 -0400

    added hound and changed layout template

--- config/test.exs
+++ config/test.exs
@@ -4,8 +4,8 @@ use Mix.Config
 # you can enable the server option below.
 config :mango, MangoWeb.Endpoint,
   http: [port: 4001],
-  server: false
-
+  server: true
+config :hound, driver: "phantomjs"
 # Print only warnings and errors during test
 config :logger, level: :warn
 
